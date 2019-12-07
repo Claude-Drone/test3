99ead376f30f5c5bff9041da1ebacd00576d81ae
Claude-Drone <56364975+Claude-Drone@users.noreply.github.com>
2019-12-07 15:34:51 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function test(){
+    document.getElementById('TXT').style.fontWeight="bold"
+}
+function test1(){
+    document.getElementById('TXT').style.fontStyle="italic"
+}
+function test2(){
+    document.getElementById('TXT').style.textDecoration="underline"
+}
+function test3(){
+    document.getElementById('TXT').style.fontSize=document.getElementById('hh').value;
+}
+function test4(){
+    document.getElementById('TXT').style.fontFamily=document.getElementById('ff').value;
+}
